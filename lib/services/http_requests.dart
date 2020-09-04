5fcd3d99aaeebe2922060c9c559bbba5153bbda3
akbarpulatov <akbarpulatov7@gmail.com>
2020-09-04 14:51:40 +0500

    got json from my json-server

--- lib/services/http_requests.dart
+++ lib/services/http_requests.dart
@@ -5,20 +5,21 @@ class HttpRequest {
   HttpRequest();
 
   void getHttp() async {
-    var url = 'http://192.168.233.211:3000/posts/2';
+    var url = 'http://192.168.233.211:3000/history-created';
 
     // Await the http get response, then decode the json-formatted response.
     var response = await http.get(url);
+    print(response.body);
 
     if (response.statusCode == 200) {
       print('status is ok!');
-      var jsonResponse = convert.jsonDecode(response.body);
+      // var jsonResponse = convert.jsonDecode(response.body);
 
-      var id = jsonResponse['id'];
-      String title = jsonResponse['title'] as String;
+      // var id = jsonResponse['id'];
+      // String title = jsonResponse['title'] as String;
 
-      print('ID of the request is $id');
-      print('Title of the request is: $title');
+      // print('ID of the request is $id');
+      // print('Title of the request is: $title');
     } else {
       print('status is NOTok!');
       print('Request failed with status: ${response.statusCode}.');
