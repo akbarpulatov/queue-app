2bdc2cdc538698b3824f1038d64568392f56a3bc
akbarpulatov <akbarpulatov7@gmail.com>
2020-09-16 21:05:52 +0500

    add mockup data to profile_view.dart

--- lib/Screens/Main/Subscreens/Profile/profile_view.dart
+++ lib/Screens/Main/Subscreens/Profile/profile_view.dart
@@ -1,12 +1,60 @@
 import 'package:flutter/material.dart';
 
+class MockBandInfo {
+  final String name;
+  final int votes;
+
+  const MockBandInfo({this.name, this.votes});
+}
+
 class ProfileViewScreen extends StatelessWidget {
   const ProfileViewScreen({
     Key key,
   }) : super(key: key);
 
+  static const List<MockBandInfo> _bandList = [
+    const MockBandInfo(name: "Name 1", votes: 1),
+    const MockBandInfo(name: "Name 2", votes: 2),
+    const MockBandInfo(name: "Name 3", votes: 3),
+    const MockBandInfo(name: "Name 4", votes: 4),
+  ];
+
+  Widget _buildListItem(BuildContext context, MockBandInfo bandInfo) {
+    return ListTile(
+      title: Row(
+        children: [
+          Expanded(
+            child: Text(
+              bandInfo.name,
+              style: Theme.of(context).textTheme.headline3,
+            ),
+          ),
+          Container(
+            decoration: const BoxDecoration(
+              color: Color(0xffddddff),
+            ),
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              bandInfo.votes.toString(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Здесь будеть страница Профилья'));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Firebase Stream'),
+      ),
+      body: ListView.builder(
+        itemExtent: 80.0,
+        itemCount: _bandList.length,
+        itemBuilder: (context, index) =>
+            _buildListItem(context, _bandList[index]),
+      ),
+    );
   }
 }
