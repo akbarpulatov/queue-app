dd733651231eb831a71d8957b81563e4d6fcbdcf
akbarpulatov <akbarpulatov7@gmail.com>
2020-10-07 12:11:30 +0500

    UserProfile data sketch is added

--- lib/Screens/Main/Subscreens/Profile/profile_view.dart
+++ lib/Screens/Main/Subscreens/Profile/profile_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auth/components/rounded_button.dart';
+import 'package:flutter_auth/constants.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
 class ProfileViewScreen extends StatelessWidget {
   const ProfileViewScreen({
@@ -9,8 +12,34 @@ class ProfileViewScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text('Личный кабинет'),
-        
+        Center(
+          child: Text('Личный кабинет'),
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Icon(
+              LineAwesomeIcons.user,
+              size: 30,
+              color: MyColors.disabled,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Login'),
+                  Text('John'),
+                  Divider(
+                    indent: 0,
+                    height: 20,
+                    endIndent: 0,
+                  ),
+                  Text('Tarif: Free'),
+                ],
+              ),
+            ),
+          ],
+        ),
       ],
     );
   }
