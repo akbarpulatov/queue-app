d70865d181a40c22b246678a2d38e0393a802880
Akbar <akbarpulatov7@gmail.com>
2020-10-19 13:11:34 +0500

    raw login function is added

--- lib/Screens/Login/components/body.dart
+++ lib/Screens/Login/components/body.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/Screens/Login/components/background.dart';
-import 'package:flutter_auth/Screens/Signup/signup_screen.dart';
 import 'package:flutter_auth/components/already_have_an_account_acheck.dart';
 import 'package:flutter_auth/components/rounded_button.dart';
 import 'package:flutter_auth/components/rounded_input_field.dart';
 import 'package:flutter_auth/components/rounded_password_field.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_auth/model/user.dart';
 
 class Body extends StatelessWidget {
+  static TextEditingController loginController = new TextEditingController();
+
   const Body({
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    loginController.text = '';
     Size size = MediaQuery.of(context).size;
     return Background(
       child: SingleChildScrollView(
@@ -26,12 +28,9 @@ class Body extends StatelessWidget {
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
             SizedBox(height: size.height * 0.03),
-            // SvgPicture.asset(
-            //   "assets/icons/login.svg",
-            //   height: size.height * 0.35,
-            // ),
             SizedBox(height: size.height * 0.03),
             RoundedInputField(
+              controller: loginController,
               hintText: "Логин",
               onChanged: (value) {},
             ),
@@ -40,7 +39,12 @@ class Body extends StatelessWidget {
             ),
             RoundedButton(
               text: "Войти",
-              press: () {},
+              press: () {
+                User.name = loginController.text;
+                User.type = UserType.user;
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                    '/', (Route<dynamic> route) => false);
+              },
             ),
             SizedBox(height: size.height * 0.03),
             AlreadyHaveAnAccountCheck(
